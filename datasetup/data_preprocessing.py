38264f678d3d42dadc4d49b89b0ca94382b7f221
jdgaudillo <joverlyn.gaudillo@digenomix.com>
2018-10-19 09:46:32 +0800

    Add function for data cleaning

--- datasetup/data_preprocessing.py
+++ datasetup/data_preprocessing.py
@@ -1,20 +1,31 @@
 import pandas as pd 
 import numpy as np
+import re
 
-from utils import *
+from datasetup.utils import *
 
 def clean(filename):
 	checkFileType(filename)
 	data = openFile(filename)
-	data = dropCols(data, features = ('PR', 'GROUP', 'YEAR'))
+	data = dropCols(data, features = ['UNNAMED: 0', 'PR', 'GROUP', 'GEOMETRY'])
 	validate(data)
 
-	data = addId(data)
-	data = dropCols(data, features = ('NAME', 'YEAR'))
+	data = addID(data)
+	data = dropCols(data, features = ['NAME', 'YEAR', 'TIME'])
+	
+	print(data.shape)
 
-	'''
-		Script to ensure the output follows the listed expected behavior of the function
+	time_steps = data['ADV'].values
+	regx = re.compile(r'[a-zA-z]$')
+	time_steps = [re.sub(regx, '', str(time_step)) for time_step in time_steps]
+	data.loc[:, 'ADV'] = time_steps
 
-	'''
+	TCID = data['TCID'].unique()
+	data = data.groupby('TCID', sort=False, group_keys=False).apply(lambda x: x.drop_duplicates('ADV', keep='last'))
 
-	toCSV(data)
+	status = data['STAT'].values
+	status = [stat.replace(' ', '') for stat in status]
+	print(data['STAT'].unique())
+
+	out_file = 'exported/Cleaned_Dataset.csv'
+	toCSV(data, out_file)
