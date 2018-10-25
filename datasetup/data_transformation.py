2c2427fb41f7c3fecc4b0b1119cfcd9926599a4a
Moonman-99 <mojhuneg38@gmail.com>
2018-10-25 15:53:34 +0800

    Automated the function of bit determination to go along with the Z-Transformation.

--- datasetup/data_transformation.py
+++ datasetup/data_transformation.py
@@ -1,6 +1,5 @@
 import pandas as pd 
 import numpy as np
-import matplotlib.pylab as plt
 
 # from utils import *
 
@@ -14,7 +13,7 @@ def PTEquivalence(data):  # input dataset, output: ONE dataset with index = TCID
     return out
 
 
-def zTransform(data, bit):  # input: dataset and desired bit, output: ONE dataset where index = TCID, and 1 column, Z
+def zTransform(data):  # input: dataset and desired bit, output: ONE dataset where index = TCID, and 1 column, Z
 
     lon_max = data['LONGITUDE'].max()
     lat_max = data['LATITUDE'].max()
@@ -37,58 +36,15 @@ def zTransform(data, bit):  # input: dataset and desired bit, output: ONE datase
     diff_lon = lon_max - lon_min
     diff_lat = lat_max - lat_min
 
-    data['Y'] = np.floor(((data['LATITUDE'] - lat_min) / diff_lat) * (2 ** bit))
-    data['X'] = np.floor(((data['LONGITUDE'] - lon_min) / diff_lon) * (2 ** bit))
+    number = range(4, 20)
 
-    X = data['X'].values.tolist()
-    Y = data['Y'].values.tolist()
-
-    z = []
-    for i in range(len(X)):
-        val = 0
-        for j in range(17):
-            val = val | ((int(X[i]) & (2 ** j)) << (j + 1))
-            val = val | ((int(Y[i]) & (2 ** j)) << (j))
-        z.append(val)
-
-    data['Z'] = z
-
-    out = data[['Z', 'TCID']].set_index('TCID')
-    return out
-
-
-# used to determine the optimal number of bit to be used in the Z-Transform
-def bitDetermination(data):  # input dataset, output plot
-    lon_max = data['LONGITUDE'].max()
-    lat_max = data['LATITUDE'].max()
-
-    lon_min = data['LONGITUDE'].min()
-    lat_min = data['LATITUDE'].min()
-
-    diff_lon = lon_max - lon_min
-    diff_lat = lat_max - lat_min
-
-    if diff_lon > diff_lat:
-        diff = diff_lon - diff_lat
-
-        lat_max += diff
-    else:
-        diff = diff_lat - diff_lon
-
-        lon_max += diff
-
-    diff_lon = lon_max - lon_min
-    diff_lat = lat_max - lat_min
-
-    bit = range(4, 20)
-
-    key_number = []
-    for num in bit:
+    value = 0
+    for num in number:
         data['Y'] = np.floor(((data['LATITUDE'] - lat_min) / diff_lat) * (2 ** num))
         data['X'] = np.floor(((data['LONGITUDE'] - lon_min) / diff_lon) * (2 ** num))
 
-        x = data['x'].values.tolist()
-        y = data['y'].values.tolist()
+        x = data['X'].values.tolist()
+        y = data['Y'].values.tolist()
 
         z = []
 
@@ -102,11 +58,27 @@ def bitDetermination(data):  # input dataset, output plot
 
         data['Z'] = z
 
-        print(num, data.Z.nunique())
-        key_number.append(data.Z.nunique())
+        if num == 4 or value != data.Z.nunique():
+            value = int(data.Z.nunique())
+        else:
+            break
 
-    plt.plot(bit, key_number, 'bx-')
-    plt.xlabel('Bit Number')
-    plt.ylabel('Number of Unique Z')
-    plt.title('NUmber of Unique Z vs Bit Number')
-    return
+    bit = num - 1
+    data['Y'] = np.floor(((data['LATITUDE'] - lat_min) / diff_lat) * (2 ** bit))
+    data['X'] = np.floor(((data['LONGITUDE'] - lon_min) / diff_lon) * (2 ** bit))
+
+    x = data['X'].values.tolist()
+    y = data['Y'].values.tolist()
+
+    z = []
+    for i in range(len(x)):
+        val = 0
+        for j in range(17):
+            val = val | ((int(x[i]) & (2 ** j)) << (j + 1))
+            val = val | ((int(y[i]) & (2 ** j)) << j)
+        z.append(val)
+
+    data['Z'] = z
+
+    out = data[['Z', 'TCID']].set_index('TCID')
+    return out
