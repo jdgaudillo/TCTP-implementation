ad10886c107f74401b216c13c21c0f180a8ad3b4
Moonman-99 <mojhuneg38@gmail.com>
2018-10-25 14:00:10 +0800

    Merge branch 'master' of C:\Users\THink\Documents\AAA University\TH\GIT\TCTP-implementation with conflicts.

--- datasetup/data_segmentation.py
+++ datasetup/data_segmentation.py
@@ -1,18 +1,11 @@
-import warnings
-warnings.filterwarnings('ignore')
-
-import matplotlib as mpl 
-mpl.use('Tkagg')
-
 import pandas as pd 
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn import metrics
-import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist
+from matplotlib import pyplot as plt
 
-import pandas as pd
-
-from datasetup.utils import *
+# from utils import *
 
 
 def kmeans(data, centroids):  # input pandas ORIGIN DataFrame, output additional row in original data set
@@ -20,26 +13,26 @@ def kmeans(data, centroids):  # input pandas ORIGIN DataFrame, output additional
     k_means = KMeans(n_clusters = centroids)
     k_means.fit(df)
 
-    centroids = k_means.cluster_centers_
+    # centroids = k_means.cluster_centers_
     labels = k_means.labels_
 
-    print(centroids)
-    print(labels)
+    # print(centroids)
+    # print(labels)
 
     data['K-Means Labels'] = labels
     return data
 
 
-#def hierarchicalClustering(data, features):
+def hierarchicalClustering(data, features):
 
 
-#def DBSCAN(data, features):
+def DBSCAN(data, features):
 
 
-def silhouetteAnalysis(data):  # input pandas ORIGIN DataFrame, output plot
+def silhouette_analysis(data):  # input pandas ORIGIN DataFrame, output plot
     data = data[['LATITUDE', 'LONGITUDE']]
     sil_list = []
-    k = range(2, 50)
+    k = range(2, 10)
     for i in k:
         k_means = KMeans(n_clusters = i)
         k_means.fit(data)
@@ -49,10 +42,26 @@ def silhouetteAnalysis(data):  # input pandas ORIGIN DataFrame, output plot
         sil_list.append(metrics.silhouette_score(data, labels, metric = 'euclidean'))
 
     # plotting for analysis
-    plt.figure()
     plt.plot(k, sil_list, 'bx-')
     plt.xlabel('Number of Centroids')
     plt.ylabel('Silhouette Score')
-    outfile = 'exported/Silhouette_Analysis_Plot_1/0.jpg'
-    plt.savefig('exported/plots/Silhouette_Analysis_Plot_filter.png')
-    plt.close()
+    plt.show()
+    return
+
+
+def elbow_plot_analysis(data):
+    data = data[['LATITUDE', 'LONGITUDE']]
+    distortions = []
+    k = range(1, 101)
+    for i in k:
+        k_means = KMeans(n_clusters = i).fit(data)
+        k_means.fit(data)
+        distortions.append(sum(np.min(cdist(data, k_means.cluster_centers_, 'euclidean'), axis = 1)) / data.shape[0])
+
+    plt.plot(k, distortions, 'rx-')
+    plt.xlabel('Number of Centroids')
+    plt.ylabel('Distortion')
+    plt.title('The Elbow Method showing the optimal Number of Centroids')
+    plt.show()
+
+    return
